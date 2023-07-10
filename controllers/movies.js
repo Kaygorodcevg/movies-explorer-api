b56b57b36693da2fb19191b14d5b1df76bd398a6
Gleb <113468160+Kaygorodcevg@users.noreply.github.com>
2023-07-10 19:46:17 +0500

    change mongoose connect

---
+++ controllers/movies.js
@@ -0,0 +1,58 @@
+const Movie = require('../models/movie');
+const { CREATED } = require('../utils/errors');
+const ForbiddenError = require('../utils/ForbiddenError');
+
+module.exports.getMovies = (req, res, next) => {
+  Movie.find({ owner: req.user._id })
+    .orFail()
+    .then((cards) => res.send(cards))
+    .catch(next);
+};
+
+module.exports.createMovie = (req, res, next) => {
+  const {
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailerLink,
+    nameRU,
+    nameEN,
+    thumbnail,
+    movieId,
+  } = req.body;
+  Movie.create({
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailerLink,
+    nameRU,
+    nameEN,
+    thumbnail,
+    movieId,
+    owner: req.user._id,
+  })
+    .then((card) => res.status(CREATED).send(card))
+    .catch(next);
+};
+
+module.exports.deleteMovie = (req, res, next) => {
+  Movie.findById(req.params.cardId)
+    .orFail()
+    .then((card) => {
+      if (card.owner.toString() === req.user._id) {
+        Movie.deleteOne(card._id)
+          .orFail()
+          .then(res.send({ message: 'Фильм удалён' }))
+          .catch(next);
+      } else {
+        next(new ForbiddenError());
+      }
+    })
+    .catch(next);
+};
