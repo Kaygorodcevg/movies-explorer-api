263ac4aac2b4fe097e26fc305daf6b2d8a449bdb
Gleb <113468160+Kaygorodcevg@users.noreply.github.com>
2023-07-10 20:21:50 +0500

    change database

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 const MODE_PRODUCTION = 'production';
 const DEV_KEY = 'dev-secret-key';
 const DEV_PORT = 3001;
-const DATABASE = 'mongodb://localhost:27017/bitfilmsdb';
+const DATABASE = 'mongodb://0.0.0.0:27017/bitfilmsdb';
 
 module.exports = {
   MODE_PRODUCTION,
